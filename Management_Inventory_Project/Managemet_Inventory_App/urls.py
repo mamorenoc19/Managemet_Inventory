d152d3fcf3a7bf1d4c95ceb7b191252fc51f80b8
Manuela Moreno <mmorenoc2@eafit.edu.co>
2021-10-23 17:32:52 -0500

    Cambios

--- Management_Inventory_Project/Managemet_Inventory_App/urls.py
+++ Management_Inventory_Project/Managemet_Inventory_App/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home),
+    path("home/", views.home, name = "home"),
+    path("search/", views.add_product, name="search"),
+    path("add/", views.add_product, name="add"),
+    path("edit/", views.edit_product, name="edit"),
+    path("remove/", views.remove_product, name="remove")
 ]
--- Management_Inventory_Project/Managemet_Inventory_App/views.py
+++ Management_Inventory_Project/Managemet_Inventory_App/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "Managemet_Inventory_App/home.html")
+
+def home(request):
+    return render(request, "Managemet_Inventory_App/search.html")
+
+def add_product(request):
+    return render(request, "Managemet_Inventory_App/inventory_add.html")
+
+def edit_product(request):
+    return render(request, "Managemet_Inventory_App/inventory_edit.html")
+
+def remove_product(request):
+    return render(request, "Managemet_Inventory_App/inventory_remove.html")
