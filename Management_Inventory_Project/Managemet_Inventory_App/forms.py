75b9f0918675b6b73c75d92664f7c4dab11c5e36
sbetancurb <69641316+sbetancurb@users.noreply.github.com>
2021-10-26 08:33:56 -0500

    Update forms.py

--- Management_Inventory_Project/Managemet_Inventory_App/forms.py
+++ Management_Inventory_Project/Managemet_Inventory_App/forms.py
@@ -1,14 +1,35 @@
 from django import forms
-from Managemet_Inventory_App.models import Producto
+from Managemet_Inventory_App.models import producto
 
 class productoForm(forms.ModelForm):
-
+    
     class Meta:
-        model = Producto
-        fields = ['nombre', 'etiqueta', 'cantidad']
-        labels = {'nombre: Nombre', 'etiqueta:Etiqueta', 'cantidad:Cantidad'}
-        widgets = {'nombre':forms.TextInput(attrs={'class':'form-control'}),
-                   'etuqueta': forms.TextInput(attrs={'class': 'form-control'}),
-                   'cantidad': forms.NumberInput(attrs={'class': 'form-control'})
+        
+        model = producto
+    
+        fields = [
+            'nombre',
+            'categoria',
+            'marca',
+            'sede',
+            'proveedor',
+            'cantidad',
+        ]
+
+        labels = {
+            'nombre':'Nombre',
+            'categoria':'Categoria',
+            'marca':'Marca',
+            'sede':'Sede',
+            'proveedor':'Proveedor',
+            'cantidad':'Cantidad', 
         }
 
+        widgets = {
+            'nombre':forms.TextInput(attrs={'class':'form-control'}),
+            'categoria':forms.TextInput(attrs={'class':'form-control'}),
+            'marca':forms.TextInput(attrs={'class':'form-control'}),
+            'sede':forms.TextInput(attrs={'class':'form-control'}),
+            'proveedor':forms.TextInput(attrs={'class':'form-control'}),
+            'cantidad':forms.NumberInput(attrs={'class':'form-control'})
+        }
