47ad1ffcedf941b5b4100c334e948d86bf94e7c0
sbetancurb <69641316+sbetancurb@users.noreply.github.com>
2021-10-25 20:51:47 -0500

    Update urls.py

--- Management_Inventory_Project/Managemet_Inventory_App/urls.py
+++ Management_Inventory_Project/Managemet_Inventory_App/urls.py
@@ -4,10 +4,10 @@ from Managemet_Inventory_App.views import home,producto_view,search,add_product,
 app_name = 'Managemet_Inventory_App'
 
 urlpatterns = [
-    path("home/", views.home, name = "home"),
-    path("productos/", views.producto_view, name="productos"),
-    path("search/", views.search, name="search"),
-    path("add/", views.add_product, name="add"),
-    path("edit/", views.edit_product, name="edit"),
-    path("remove/", views.remove_product, name="remove")
+    path("home/", home, name = "home"),
+    path("productos/", producto_view, name="productos"),
+    path("search/", search, name="search"),
+    path("add/", add_product, name="add"),
+    path("edit/", edit_product, name="edit"),
+    path("remove/", remove_product, name="remove")
 ]
