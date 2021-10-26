19f39495c8a658ed50c14b27b26708883f64f099
sbetancurb <69641316+sbetancurb@users.noreply.github.com>
2021-10-26 08:32:58 -0500

    Update views.py

--- Management_Inventory_Project/Managemet_Inventory_App/views.py
+++ Management_Inventory_Project/Managemet_Inventory_App/views.py
@@ -9,25 +9,23 @@ def home(request):
 def search(request):
     return render(request, "Managemet_Inventory_App/search.html")
 
-def producto_view(request):
+def inventory(request):
+    return render(request, "Managemet_Inventory_App/inventory.html")
+
+def add_product(request):
     if request.method == 'POST':
         form = productoForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('Managemet_Inventory_App:productos')
+        return redirect('Managemet_Inventory_App:inventory')
     else:
         form = productoForm()
     
-    return render(request, "Managemet_Inventory_App/productos.html", {'form':form})
-
-def home(request):
-    return render(request, "Managemet_Inventory_App/search.html")
-
-def add_product(request):
-    return render(request, "Managemet_Inventory_App/inventory_add.html")
+    return render(request, "Managemet_Inventory_App/add.html", {'form':form})
 
 def edit_product(request):
     return render(request, "Managemet_Inventory_App/inventory_edit.html")
 
 def remove_product(request):
     return render(request, "Managemet_Inventory_App/inventory_remove.html")
+    
