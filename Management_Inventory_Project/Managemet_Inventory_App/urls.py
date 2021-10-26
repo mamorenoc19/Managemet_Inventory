c1e85f5fd1d9cba56127eedeb668bbf3e49b3fc4
sbetancurb <69641316+sbetancurb@users.noreply.github.com>
2021-10-26 08:33:33 -0500

    Update urls.py

--- Management_Inventory_Project/Managemet_Inventory_App/urls.py
+++ Management_Inventory_Project/Managemet_Inventory_App/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from Managemet_Inventory_App.views import home,producto_view,search,add_product,edit_product,remove_product
+from Managemet_Inventory_App.views import home,search,add_product,edit_product,remove_product,inventory
 
 app_name = 'Managemet_Inventory_App'
 
 urlpatterns = [
     path("home/", home, name = "home"),
-    path("productos/", producto_view, name="productos"),
+    path("inventory/",inventory,name = "inventory"),
     path("search/", search, name="search"),
     path("add/", add_product, name="add"),
     path("edit/", edit_product, name="edit"),
